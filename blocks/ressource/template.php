be86e01fc3dce96dbc8e8cf712a4893bf9d1d4e9
Mathilde Arconte <signup@marconte.fr>
2025-06-25 18:42:28 +0200

    Correction de l'appel à get_field dans le bloc Ressource pour améliorer la récupération des données et ajout de l'affichage de l'image à la une.

--- blocks/ressource/template.php
+++ blocks/ressource/template.php
@@ -3,10 +3,12 @@
 /**
  * Template for the Ressource Block
  */
-$data = get_field('template', false, false);
+$data = get_field('template');
+$post_img = get_field('template', false, false);
 var_dump($data); // For debugging purposes, remove in production
 ?>
 
 <div id="<?php echo esc_attr($block_id); ?>" class="<?php echo esc_attr($class_name); ?>">
 	BLOCK
+	<?php echo get_the_post_thumbnail($post_img, 'thumbnail'); ?>
 </div>

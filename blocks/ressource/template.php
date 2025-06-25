12013bf2226568592d076eae511cc7573e62ecbd
Mathilde Arconte <signup@marconte.fr>
2025-06-25 19:27:39 +0200

    Ajout de styles CSS pour les titres et les boutons dans le bloc Ressource, y compris une nouvelle police et un dégradé pour le séparateur.

--- blocks/ressource/template.php
+++ blocks/ressource/template.php
@@ -23,8 +23,8 @@ $post_img = get_field('template', false, false);
 			?></h2>
 		<p><?php echo esc_html($data->post_excerpt);
 			?></p>
-		<a class="btn" href="/ressources/templates/<?php echo esc_url($data->post_name);
-													?>">Télécharger gratuitement le modèle</a>
+		<a class="btn btn-dev" href="/ressources/templates/<?php echo esc_url($data->post_name);
+															?>">Télécharger gratuitement le modèle</a>
 	</div>
 	<?php echo get_the_post_thumbnail($post_img, 'full'); ?>
 </section>
